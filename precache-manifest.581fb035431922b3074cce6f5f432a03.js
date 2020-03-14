601a6455b5074df2a993b537c8d0deb833a2d0c2
Tungtwister <austin.tung56@gmail.com>
2020-03-14 03:18:17 -0700

    Updates

---
+++ precache-manifest.581fb035431922b3074cce6f5f432a03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7984c706f44120f08f43a733f852c8b3",
+    "url": "/yelpClone/index.html"
+  },
+  {
+    "revision": "58cbb4de1eac1bc25733",
+    "url": "/yelpClone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48f3578cd001f6ce17b3",
+    "url": "/yelpClone/static/js/2.ba7999fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/yelpClone/static/js/2.ba7999fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cbb4de1eac1bc25733",
+    "url": "/yelpClone/static/js/main.5a8e4dd3.chunk.js"
+  },
+  {
+    "revision": "dea7c4f07586bfb11543",
+    "url": "/yelpClone/static/js/runtime-main.fc6e7e99.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yelpClone/static/media/logo.5d5d9eef.svg"
+  }
+]);
